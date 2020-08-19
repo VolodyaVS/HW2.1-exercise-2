551437cf08ba8aa4ec03edb606a7e78ae9ba6d3a
Volodya <volodyavolga@protonmail.ch>
2020-08-20 02:06:12 +0300

    the end

--- HW2.1 exercise 2/ViewController.swift
+++ HW2.1 exercise 2/ViewController.swift
@@ -9,12 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var redOutlet: UIView!
+    @IBOutlet weak var yellowOutlet: UIView!
+    @IBOutlet weak var greenOutlet: UIView!
+    
+    @IBOutlet weak var buttonOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redOutlet.layer.cornerRadius = redOutlet.layer.bounds.size.width / 2
+        yellowOutlet.layer.cornerRadius = yellowOutlet.layer.bounds.size.width / 2
+        greenOutlet.layer.cornerRadius = greenOutlet.layer.bounds.size.width / 2
+        
+        buttonOutlet.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        
+        if redOutlet.alpha != 1,
+            yellowOutlet.alpha != 1,
+            greenOutlet.alpha != 1 {
+            redOutlet.alpha = 1
+        } else if redOutlet.alpha == 1 {
+            redOutlet.alpha = 0.3
+            yellowOutlet.alpha = 1
+        } else if yellowOutlet.alpha == 1 {
+            yellowOutlet.alpha = 0.3
+            greenOutlet.alpha = 1
+        } else {
+            greenOutlet.alpha = 0.3
+            redOutlet.alpha = 1
+        }
+        buttonOutlet.setTitle("NEXT", for: .normal)
+    }
+    
 }
 
